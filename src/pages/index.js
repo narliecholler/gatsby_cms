0d97d836619185aec31f8197893ecee2eac9920b
Charlie Noller <charlie.noller@bglgroup.co.uk>
2021-01-13 21:12:06 +0000

    updating coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Heading, Box } from '@chakra-ui/core';
 
 export default () => (
-  <div>
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', width: '100vw', backgroundColor: '#f8f2e7', color: '#861f41' }}>
     <Box>
-      <Heading as="h1" size="xl">Site coming soon!</Heading>
+      <Heading as="h1" size="xl">Site is currently in development, hang tight!</Heading>
     </Box>
-  </div>
+  </div >
 )
